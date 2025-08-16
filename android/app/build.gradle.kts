3f310a1b238546863d41519dad8707331b777446
{jinsung} <jayhan0215@gmail.com>
2025-08-16 14:40:20 +0900

    Delete firebase appbundle for android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+  implementation("com.google.firebase:firebase-analytics")
+}
+
 android {
     namespace = "com.jay.app_sticker_note"
     compileSdk = flutter.compileSdkVersion
@@ -20,10 +25,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.jay.app_sticker_note"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +34,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
